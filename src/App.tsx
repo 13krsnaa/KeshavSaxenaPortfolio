d8912bd442039f5c60b4b2a4efcb977514d81704
13krsnaa <saxenakrishna9140@gmail.com>
2026-02-05 21:37:33 +0530

    added ClickSpark

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,30 @@ import Experience from "./components/Experience";
 import Projects from "./components/Project";
 import Contact from "./components/Contact";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import ClickSpark from "./components/ClickSpark";
 
 function App() {
 
   return (
     <ThemeProvider>
-      <div className="min-h-screen">
-        <Navigation />
-        <Hero />
-        <About />
-        <Experience />
-        <Projects />
-        <Contact />
-      </div>
+      <ClickSpark
+        sparkColor="#ffffff"
+        sparkSize={16}
+        sparkRadius={31}
+        sparkCount={8}
+        duration={400}
+        easing="ease-out"
+        extraScale={1}
+      >
+        <div className="min-h-screen">
+          <Navigation />
+          <Hero />
+          <About />
+          <Experience />
+          <Projects />
+          <Contact />
+        </div>
+      </ClickSpark>
     </ThemeProvider>
   )
 }
